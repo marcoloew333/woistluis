1bac26bc56b67e4549ddc7073b64c2b8ebbf6f9e
Marco Loew <marcoloew333@gmail.com>
2019-08-03 18:35:18 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    bets: [],
+    bet: {
+      person_name: "Peter Pan",
+      time_bet: "10:17 Uhr"
+    }
+  };
+
+  componentDidMount() {
+    this.getBets();
+  }
+
+  getBets = _ => {
+    fetch("http://www.woistluis.moodlions.de/bets") //http://www.woistluis.moodlions.de/bets //http://192.168.178.198:4000
+        .then(response => response.json())
+        .then(response => this.setState({bets: response.data}))
+        .catch(err => console.error(err))
+  };
+
+  addBet = _ => {
+    const { bet } = this.state;
+
+    fetch(`http://www.woistluis.moodlions.de/bets/add?name=${bet.person_name}&bet=${bet.time_bet}`) //http://www.woistluis.moodlions.de/bets
+        .then(response => response.json())
+        .then(this.getProducts)
+        .catch(err => console.error(err))
+  };
+
+  renderBets = ({ bet_id, name, bet}) =>
+      <div key={bet_id}>
+        <p>{name}</p>
+        <p>{bet}</p>
+      </div>;
+
+    render() {
+    const { bets, bet } = this.state;
+
+        return (
+            <div>
+              {bets.map(this.renderBets)}
+              <div>
+                <input
+                    value={bet.person_name}
+                    onChange={e => this.setState({bet: { ...bet, person_name: e.target.value}})}
+                />
+                <input
+                    value={bet.time_bet}
+                    onChange={e => this.setState({bet: { ...bet, time_bet: e.target.value}})}
+                />
+                <button onClick={this.addBet}>Wette abgeben</button>
+              </div>
+            </div>
+        )
+    }
 }
 
 export default App;
