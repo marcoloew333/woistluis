6a7c9ea32d47d0abaaa7673f7c7d22d35dec7d65
Marco Loew <marcoloew333@gmail.com>
2019-08-09 22:35:54 +0200

    Changed a lot 46

---
+++ Client/build/precache-manifest.00f0616057a4784e8c5238a4d7a9dbd7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c68bb833ec07629408bf65e7c0fd3ed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f065610b5e4d76e14ed",
+    "url": "/static/css/main.d99fbbcb.chunk.css"
+  },
+  {
+    "revision": "7c1e8cde0f1f57d059dd",
+    "url": "/static/js/2.7e675ef3.chunk.js"
+  },
+  {
+    "revision": "0f065610b5e4d76e14ed",
+    "url": "/static/js/main.a2bf86bb.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
