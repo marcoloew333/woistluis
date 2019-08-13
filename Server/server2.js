60c157bd7ad097b610c976b64166e408b052cf3d
Marco Loew <marcoloew333@gmail.com>
2019-08-13 21:59:24 +0200

    Changed a lot 99

--- Server/server2.js
+++ Server/server2.js
@@ -57,7 +57,7 @@ app.get("/bets", (req, res) => {
 app.get("/bets/add", (req, res) => {
     const {name, bet} = req.query;
     const NAME_CHECK = `SELECT bet, COUNT(*) as cnt FROM bets WHERE name = "${name}"`;
-    const INSERT_BET = `INSERT INTO bets (name, bet, timestamp) VALUES(?,?,CURTIME()+2:00)`;
+    const INSERT_BET = `SET TIME ZONE +2:00 INSERT INTO bets (name, bet, timestamp) VALUES(?,?,CURTIME())`;
     const UPDATE_BET = `UPDATE bets SET bet=? WHERE name=?`;
     if (connection.query(NAME_CHECK)) {
         connection.query(UPDATE_BET, [bet, name], (err, result) => {
